10af293312408f2d9e2b27b00982f1e44b834fc0
Benjamin Lacroix <blacroix@xebia.fr>
2019-11-28 11:32:00 +0100

    use method instead of arrow function

--- xebi-spoti/src/components/organisms/TechTrendCategory.vue
+++ xebi-spoti/src/components/organisms/TechTrendCategory.vue
@@ -36,15 +36,18 @@
       }
     },
     methods: {
-      shuffle: (list) => { // https://stackoverflow.com/a/12646864
-        for (let i = list.length() - 1; i > 0; i--) {
+      shuffle(list) { // https://stackoverflow.com/a/12646864
+        for (let i = list.length - 1; i > 0; i--) {
           const j = Math.floor(Math.random() * (i + 1));
           [list[i], list[j]] = [list[j], list[i]];
         }
+        return list;
       }
     },
     computed: {
-      randomTechTrends: () => this.shuffle([...this.techTrends])
+      randomTechTrends() {
+        return this.shuffle([...this.techTrends])
+      }
     }
   }
 </script>
